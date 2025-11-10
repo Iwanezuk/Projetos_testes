0d04c525c1df4ca65cad4e7bf55414e6f0ffb528
Iwanezuk <iwanezuk@hotmail.com>
2025-11-10 18:55:36 -0300

    Update App.jsx

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,16 +1,18 @@
-// import './App.css';
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
-// import './assets/css/base/base.css';
+import './assets/css/base/base.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
 
 function App() {
   return (
-    <>
-      <Home />
-      <Sobre />
-    </>
-  );
+    <Router>
+      <Routes>
+        {/* Use element={<Componente />} para renderizar */}
+        <Route path='/' element={<Home />} /> 
+        <Route path='/sobre' element={<Sobre />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
