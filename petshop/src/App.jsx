216883b2c665699c3ac17cbec5ff7c3c9715d2fc
Iwanezuk <iwanezuk@hotmail.com>
2025-11-10 22:20:33 -0300

    new file:   cap02_finalizado.zip 	modified:   petshop/src/App.jsx 	new file:   petshop/src/assets/css/404.css 	new file:   petshop/src/assets/css/components/cabecalho.css 	new file:   petshop/src/assets/img/catiorrinho.svg 	new file:   petshop/src/assets/img/catiorrinho404.svg 	new file:   petshop/src/assets/img/menu.svg 	new file:   petshop/src/paginas/Pagina404.jsx

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,7 +1,10 @@
-import Home from './paginas/Home';
-import Sobre from './paginas/Sobre';
+
 import './assets/css/base/base.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
+
+import Home from './paginas/Home';
+import Sobre from './paginas/Sobre';
+import Pagina404 from './paginas/Pagina404';
 
 function App() {
   return (
@@ -10,6 +13,7 @@ function App() {
         {/* Use element={<Componente />} para renderizar */}
         <Route path='/' element={<Home />} /> 
         <Route path='/sobre' element={<Sobre />} />
+        <Route path='*' element={<Pagina404 />} />
       </Routes>
     </Router>
   )
