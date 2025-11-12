4340bab309e3a8497eafc9d4c40b63d531028fa7
Iwanezuk <iwanezuk@hotmail.com>
2025-11-12 00:53:31 -0300

    Update Home.jsx

--- petshop/src/paginas/Home.jsx
+++ petshop/src/paginas/Home.jsx
@@ -1,3 +1,4 @@
+import ListaCategorias from "../components/ListaCategorias";
 import ListaPost from "../components/ListaPost";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
             <div className="container">
                 <h2 className="titulo-pagina">Pet notícias</h2>
             </div>
+            <ListaCategorias />
             <ListaPost url={'/posts'} />
         </main>
     )
